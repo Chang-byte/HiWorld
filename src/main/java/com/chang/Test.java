5993ce289a00d232fa41e07398c6b0758379571a
chang <1241236298@qq.com>
2023-03-04 16:38:19 +0800

    third commit

--- src/main/java/com/chang/Test.java
+++ src/main/java/com/chang/Test.java
@@ -5,6 +5,8 @@ public class Test {
         /*
             红色： 还未添加到暂存区，在总文件右键 git add 会变成绿色
             绿色： 已经添加到暂存区, 使用commit 添加到本地库
+            黑色： 一旦提交后，会变成黑色。
+
          */
         System.out.println(1);
         System.out.println(2);
