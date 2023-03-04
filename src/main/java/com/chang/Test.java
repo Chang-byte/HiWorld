22af0cc4a80bb531f10e61e36897ef18b92d7753
chang <1241236298@qq.com>
2023-03-04 16:29:53 +0800

    init1

--- src/main/java/com/chang/Test.java
+++ src/main/java/com/chang/Test.java
@@ -2,6 +2,10 @@ package com.chang;
 
 public class Test {
     public static void main(String[] args) {
+        /*
+            红色： 还未添加到暂存区，在总文件右键 git add 会变成绿色
+            绿色： 已经添加到暂存区, 使用commit 添加到本地库
+         */
         System.out.println(1);
     }
 }
---
+++ src/main/java/com/chang/Test01.java
@@ -0,0 +1,4 @@
+package com.chang;
+
+public class Test01 {
+}
